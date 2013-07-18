a446ea06110e3a68e01ec6bd6d212cdeac1b7647
Andy Kilinskis <andrewkilinskis@gmail.com>
2013-07-18 13:33:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoLFantasy::Application.config.secret_key_base = 'f3de8210c6add219b189e945cf27d142fe2e1b1ef4c10d2beb58a1076c612ab953335cde084f7e5fe92fd16effa1f04d5b3dfeab2ca73ff1291ab34df81020b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoLFantasy::Application.config.session_store :cookie_store, key: '_LoLFantasy_session'
